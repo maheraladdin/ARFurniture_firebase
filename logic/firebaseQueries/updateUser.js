95f1240a82038fb0e6be785c172bdb91f528f601
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2023-05-01 20:28:52 +0300

    UPD updateUser.js

--- logic/firebaseQueries/updateUser.js
+++ logic/firebaseQueries/updateUser.js
@@ -8,7 +8,7 @@ const updateUser = (data) => {
     updateEmail(auth.currentUser, data.email).then(async () => {
         const userDoc = await doc(db, `users`, isLogin.uid);
         isLogin.updateProfile = data;
-        await updateDoc(userDoc, data)
+        await updateDoc(userDoc, data);
     }).catch((error) => {
         console.log(error);
     });
